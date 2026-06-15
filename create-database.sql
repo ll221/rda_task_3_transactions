883e163f163588dd09d7f8dd740f0a62630f126c
ll221 <ihor.kunakh.kb.2022@lpnu.ua>
2026-06-16 00:17:22 +0300

    4

--- create-database.sql
+++ create-database.sql
@@ -1,44 +1,35 @@
+DROP DATABASE IF EXISTS ShopDB;
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Products (
-    ID INT AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(50),
     Description VARCHAR(100),
     Price INT,
-    WarehouseAmount INT,
-    PRIMARY KEY (ID)
+    WarehouseAmount INT
 );
 
 CREATE TABLE Customers (
-    ID INT AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(50),
     LastName VARCHAR(50),
-    Email VARCHAR(50),
-    Address VARCHAR(50),
-    PRIMARY KEY (ID)
+    Email VARCHAR(100),
+    Address VARCHAR(50)
 );
 
 CREATE TABLE Orders (
-    ID INT AUTO_INCREMENT,
-    CustomerID INT, 
-    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT,
     Date DATE,
-    PRIMARY KEY (ID)
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
 );
 
 CREATE TABLE OrderItems (
-    ID INT AUTO_INCREMENT,
-    OrderID INT, 
-    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
-	ProductID INT, 
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
     Count INT,
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL,
-    PRIMARY KEY (ID)
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
 );
-
-INSERT INTO Products (Name, Description, Price, WarehouseAmount)
-	VALUES ('AwersomeProduct', 'Product Desctiption', 5, 42);
-
-INSERT INTO Customers (FirstName, LastName, Email, Address)
-	VALUES ('John', 'Dou', 'j@dou.ua', 'far, far away');
