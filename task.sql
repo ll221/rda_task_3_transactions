bbed57a8961d2aed82a6902dcaabee837935f329
ll221 <ihor.kunakh.kb.2022@lpnu.ua>
2026-06-16 00:23:07 +0300

    7

--- task.sql
+++ task.sql
@@ -9,6 +9,8 @@ INSERT INTO Customers (ID, FirstName, LastName, Email, Address)
 VALUES (1, 'John', 'Doe', 'john.doe@example.com', '123 Main St');
 
 -- Start the transaction 
+START TRANSACTION;
+
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
@@ -18,6 +20,5 @@ VALUES (LAST_INSERT_ID(), 1, 1);
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
 WHERE ID = 1;
--- And some data should be created inside the transaction 
 
-COMMIT; 
+COMMIT;
