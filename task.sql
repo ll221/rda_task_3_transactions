c67b2186083c09bcc48ec3785abce97fcf80bf8c
ll221 <ihor.kunakh.kb.2022@lpnu.ua>
2026-06-16 00:09:07 +0300

    1

--- task.sql
+++ task.sql
@@ -5,6 +5,12 @@ USE ShopDB;
 
 -- Start the transaction 
 START TRANSACTION; 
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
+
+INSERT INTO OrderItems (OrderID, ProductID)
+VALUES (LAST_INSERT_ID(), 1);
+
 
 -- And some data should be created inside the transaction 
 
